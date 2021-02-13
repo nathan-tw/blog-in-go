5a086afdbf5281a01e14ddf6f10b96c2fb419dd1
nathan-tw <106306027@nccu.edu.tw>
2021-02-13 08:17:46 -0500

    initial commit

---
+++ internal/model/article_tag.go
@@ -0,0 +1,9 @@
+type ArticleTag struct {
+	*Model
+	TagID     uint32 `json:"tag_id"`
+	ArticleID uint32 `json:"article_id"`
+}
+
+func (a Article) TableName() string {
+	return "blog_article_tag"
+}
